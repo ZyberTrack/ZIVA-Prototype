ad030124285b9da2f5b5692664ae4be85f521c19
Viktor Olenberg <115556179+ZyberTrack@users.noreply.github.com>
2025-11-23 12:38:07 +0100

    Umstellung auf VS2026 und .NET10 Entfernen von IOS, Android support

--- bin/Debug/net8.0-windows10.0.19041.0/win10-x64/AppX/wwwroot/js/timeinterval-zoom-handler.js
+++
@@ -1,9 +0,0 @@
-window.registerZoomHandler = (dotnetHelper) => {
-    window.addEventListener('wheel', function (e) {
-        if (e.ctrlKey) {
-            e.preventDefault(); // verhindert Browser-Zoom
-            const direction = e.deltaY < 0 ? "in" : "out";
-            dotnetHelper.invokeMethodAsync('OnZoom', direction);
-        }
-    }, { passive: false });
-};
