3bc258fbf35f81b9418f3600ff89abbdd8c8f136
Viktor Olenberg <115556179+ZyberTrack@users.noreply.github.com>
2025-12-03 15:53:10 +0100

    Feat: Domänen werden in Farben Sortiert und dargestellt Feat: Einfache Domänen Zusammenfassung in dürftiger Ausführung Implementiert.

---
+++ Components/Models/DomainEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZIVA_Prototype.Components.Models
+{
+    public class DomainEntry
+    {
+        public string Domain { get; set; }
+        public string Url { get; set; }
+        public DateTime VisitTime { get; set; }
+        public bool IsDirect { get; set; } // true = direkter Aufruf
+        public bool IsSearch { get; set; } // true = Suchmaschine
+        public bool IsExpanded { get; set; } = false;
+        public List<BrowserHistoryEntry> SubEntries { get; set; } = new();
+        public int Position { get; set; } = 0; // für Timeline
+    }
+
+}
