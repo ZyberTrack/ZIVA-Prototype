d7b40c7c50bf9943bc530e226eca6d2bc8cd2c9c
Viktor Olenberg <115556179+ZyberTrack@users.noreply.github.com>
2026-05-17 16:15:23 +0200

    feat: Artefact Table lässt sich jetzt hoch und runter ziehen. fix: Artefact Table jetzt standardmäßig eingeblendet.

---
+++ wwwroot/js/ResizeHandler.js
@@ -0,0 +1,36 @@
+﻿window.artifactResize = {
+
+    start: function (dotnet) {
+
+        document.body.style.userSelect = "none";
+        document.body.style.cursor = "ns-resize";
+
+        function move(e) {
+
+            const height =
+                window.innerHeight - e.clientY;
+
+            dotnet.invokeMethodAsync(
+                'ResizeArtifactTable',
+                height);
+        }
+
+        function up() {
+
+            document.body.style.userSelect = "";
+            document.body.style.cursor = "";
+
+            window.removeEventListener(
+                'mousemove', move);
+
+            window.removeEventListener(
+                'mouseup', up);
+        }
+
+        window.addEventListener(
+            'mousemove', move);
+
+        window.addEventListener(
+            'mouseup', up);
+    }
+};
